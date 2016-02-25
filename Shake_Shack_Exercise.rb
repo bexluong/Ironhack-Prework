67d23d1e7ae8e40095031d470b00e4a85b549cac
bexsekai <rebluong@gmail.com>
2016-02-25 17:07:58 -0500

    added milkshake class and ingredient class

--- Shake_Shack_Exercise.rb
+++ Shake_Shack_Exercise.rb
@@ -1,13 +1,33 @@
 class MilkShake
-  def initialize(flavor, price)
-    @flavor = flavor
-    @price = price
+  def initialize
+    @base_price = 3
+    @ingredients = [ ]    
   end
+
+	def price_of_milkshake
+  #Let's establish what our counter should be before we start adding ingredients into the mix
+  total_price_of_milkshake = @base_price
+  #Add each ingredients price to our total
+  @ingredients.each do |ingredient|
+    total_price_of_milkshake += ingredient.price
+  end
+  #return  our total price to whoever called for it
+   total_price_of_milkshake
 end
-milkshake = MilkShake.new("Chocolate", 7)
+end
+
 class Ingredient
+  attr_reader :name, :price
   def initialize(name, price)
-    @name = name
-    @price = price
+      @name = name
+      @price = price
   end
 end
+
+nizars_milkshake = MilkShake.new
+banana = Ingredient.new("Banana", 2)
+chocolate_chips = Ingredient.new("Chocolate Chips", 1)
+nizars_milkshake.add_ingredient(banana)
+nizars_milkshake.add_ingredient(chocolate_chips)
+
+puts nizars_milkshake.price_of_milkshake
