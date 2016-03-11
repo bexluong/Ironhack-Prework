a98e677410df619221dedf1928604c922ba4d504
bexsekai <rebluong@gmail.com>
2016-03-11 13:49:16 +0100

    create file file for shopping cart exercise

---
+++ ShoppingCart/shopping_cart.rb
@@ -0,0 +1,50 @@
+class ShoppingCart
+  def initialize
+    @items = []
+  end
+end
+=begin
+By default, items will have no discount.
+Fruits have no discount during weekdays but on weekends they have a 10% discount.
+Housewares have no discount unless their unit price is more than 100€ / $100. 
+If the price is greater than 100€ / $100, there will be a 5% discount.
+
+=end
+
+class Item 
+  def initialize(name, price)
+      @name = name
+      @price = price
+  end
+
+  def price
+      #Your beautiful code goes here
+  end
+end
+
+class Houseware < Item
+
+  def price
+      #Hmmm maybe this changes somehow..
+      if price > 100
+      	final_price = @price * 0.95
+      else
+      	final_price = @price
+  end
+end
+
+class Fruit < Item
+  def price
+      #Something special may go here too...
+
+    t = Time.now
+    if 
+	t.saturday?  = true
+	final_price = @price * 0.9
+	elsif 
+	t.sunday? = true
+	final_price = @price * 0.9
+	else
+	final_price = @price
+  end
+end
