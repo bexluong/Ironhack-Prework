52c1e2cc4439763d98e1b53abfae60930829b0a0
bexsekai <rebluong@gmail.com>
2016-03-18 15:20:23 +0100

    update solve cipher

--- CaesarsCipher/solve_cipher.rb
+++ CaesarsCipher/solve_cipher.rb
@@ -1,4 +1,5 @@
 require 'pry'
+
 #first iteration
 =begin
 	
@@ -14,10 +15,19 @@ solved.join
 =end
 
 #second iteration
-binding.pry
-def solve_cipher(input, key)
-    return input.chars.map { |c| (c.ord-key).chr}
+
+BASE_ORD = 'A'.ord
+
+def solve_cipher(phrase, key)
+	#gsub matches only letters of the alphabet
+  phrase.gsub(/[a-z]/i) do |letter|
+  	#subs the letters, caplocks it, pulls ord num and subtracts it from base num
+    orig_phrase = letter.upcase.ord - BASE_ORD
+    #numbers and adds key, then helps loop back around with subtracting what's left from 26
+    solved_phrase = (orig_phrase + key) % 26
+    (solved_phrase + BASE_ORD).chr
+  end
 end
 
-solved = solve_cipher("ifmmp", -1)
-solved.join
+solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
+
